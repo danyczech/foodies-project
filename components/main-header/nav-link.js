5ded9583ce3ed27b78a2e91cf640f2caa9d5558a
danakal <dana.kalcikova@centrum.cz>
2024-01-15 20:02:58 +0100

    use client components

---
+++ components/main-header/nav-link.js
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./nav-link.module.css";
+
+const NavLink = ({href, children}) => {
+    const path = usePathname();
+return(
+    <Link href={href}
+     className={path.startsWith(href) ? `${styles.link} ${styles.active}` : styles.active }>
+        {children}
+    </Link>
+)
+}
+
+export default NavLink;
