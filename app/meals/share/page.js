a7695d44cdc9b3ce0e5cf77556208efde377136a
danakal <dana.kalcikova@centrum.cz>
2024-01-31 20:48:29 +0100

    Meals Page added

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,5 @@
 const MealsShare = () => {
 return <h1>Meals Share Page</h1>
 } 
+
+export default MealsShare;
--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,14 +1,16 @@
 "use client";
 
 import Link from "next/link";
+import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import styles from "./nav-link.module.css";
 
 const NavLink = ({href, children}) => {
     const path = usePathname();
 return(
-    <Link href={href}
-     className={path.startsWith(href) ? `${styles.link} ${styles.active}` : styles.active }>
+    <Link href={href} className={path.startsWith(href) 
+    ? clsx(styles.link, styles.active) 
+    : styles.link }>
         {children}
     </Link>
 )
