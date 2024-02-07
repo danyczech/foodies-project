f2516bcad1519d2245e88362ecc1863c870cec58
danakal <dana.kalcikova@centrum.cz>
2024-02-07 14:51:55 +0100

    server-side form reading

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,19 @@ import ImagePicker from '@/components/meals/image-picker';
 import styles from './page.module.css';
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) { //+ action property in form element
+    'use server'; //it executes only on server
+
+    const meal = {
+      title: formData.get('title'), //connected via name property in input element
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email')
+    }
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -11,7 +24,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +52,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
